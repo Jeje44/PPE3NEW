45257a8fac8a38a82a90a0a6aeeaa52255b91a50
Jérémy <jeremyvilais@outlook.fr>
2019-10-15 17:18:51 +0200

    push fin de scéance

--- FChangerMdp.Designer.cs
+++ FChangerMdp.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbMDP = new System.Windows.Forms.TextBox();
+            this.tbNewMdp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbNewMDP2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbMDP
+            // 
+            this.tbMDP.Location = new System.Drawing.Point(305, 66);
+            this.tbMDP.Name = "tbMDP";
+            this.tbMDP.Size = new System.Drawing.Size(100, 20);
+            this.tbMDP.TabIndex = 0;
+            // 
+            // tbNewMdp
+            // 
+            this.tbNewMdp.Location = new System.Drawing.Point(305, 104);
+            this.tbNewMdp.Name = "tbNewMdp";
+            this.tbNewMdp.Size = new System.Drawing.Size(100, 20);
+            this.tbNewMdp.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mot de passe Actuel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Saisir nouveau mot de passe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(150, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Retaper le mot de passe";
+            // 
+            // tbNewMDP2
+            // 
+            this.tbNewMDP2.Location = new System.Drawing.Point(305, 145);
+            this.tbNewMDP2.Name = "tbNewMDP2";
+            this.tbNewMDP2.Size = new System.Drawing.Size(100, 20);
+            this.tbNewMDP2.TabIndex = 5;
+            // 
+            // FChangerMdp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNewMDP2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbNewMdp);
+            this.Controls.Add(this.tbMDP);
+            this.Name = "FChangerMdp";
             this.Text = "FChangerMdp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbMDP;
+        private System.Windows.Forms.TextBox tbNewMdp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbNewMDP2;
     }
 }
